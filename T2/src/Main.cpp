cabed5f707a8b4bbf08b7a6c740d4096931dce3c
matheustomieiro <matheustomieiro@usp.com.br>
2019-12-01 14:51:59 -0300

    Esta num andamento excelente

--- T2/src/Main.cpp
+++ T2/src/Main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<bits/stdc++.h> 
 #include "../lib/Matriz.h"
 #include <iostream>
 using namespace std;
@@ -12,13 +13,35 @@ Portanto a matriz de memorizacao conterah como primeiro
 indice o degrau_atual e como segundo degraus_restantes
 --------------------------------*/
 int **MEMO;
-int N;
+int INF = __INT_MAX__;
+int N, Count = 0;
+
+int max_com_restricoes(){
+
+}
+
+int degraus(int degrau_atual, int degraus_restantes){
+    if(degraus_restantes<0) return -INF;
+    if(degrau_atual == N) return 0;
+    if(MEMO[degrau_atual][degraus_restantes] != -1){
+        Count++;
+        return MEMO[degrau_atual][degraus_restantes];
+    }
+    //TODO: SUBSTITUIR max por max_com_restricoes
+    MEMO[degrau_atual][degraus_restantes] = max(degraus(degrau_atual+1,degraus_restantes-1),degraus(degrau_atual+2,degraus_restantes-2));
+    if(degrau_atual == 0){
+        return Count;
+    }
+    Count++;
+    return MEMO[degrau_atual][degraus_restantes];
+}
+
 
 int main(int argc, char *argv[]){
     printf("Digite o numero de degraus: ");
     scanf("%d",&N);
     MEMO = aloca_m_quad(N);
-    printa_matriz(MEMO, N);
+    printf("Numero de possibilidades: %d\n",degraus(0,N-1));
     libera_m_quad(MEMO, N);
     return 0;
 }
