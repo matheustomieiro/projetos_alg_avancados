dc0b312a89c1c22f3e947c5f405d0ff79e3805b1
matheustomieiro <matheustomieiro@usp.br>
2019-12-04 22:40:07 -0300

    Antes da mudanca drastica

--- T2/src/Main.cpp
+++ T2/src/Main.cpp
@@ -14,21 +14,28 @@ indice o degrau_atual e como segundo degraus_restantes
 --------------------------------*/
 int **MEMO;
 int INF = __INT_MAX__;
-int N, Count = 0;
+int N, NR, Max = -INF, aux, Count = 0;
+int *VARG;
 
 int degraus(int degrau_atual, int degraus_restantes){
     if(degraus_restantes<0) return -INF;
-    if(degrau_atual == N) return 0;
+    if(degrau_atual == N) return 1;
     if(MEMO[degrau_atual][degraus_restantes] != -1){
         Count++;
         return MEMO[degrau_atual][degraus_restantes];
     }
-    //TODO: SUBSTITUIR max por max_com_restricoes
-    MEMO[degrau_atual][degraus_restantes] = max(degraus(degrau_atual+1,degraus_restantes-1),degraus(degrau_atual+2,degraus_restantes-2));
+    for(int i=0; i<NR; i++){
+        aux = degraus(degrau_atual+VARG[i], degraus_restantes-VARG[i]);
+        if(aux > Max){
+            Count++;
+            Max = aux;
+        }
+    }
+    Count++;
+    MEMO[degrau_atual][degraus_restantes] = Max;
     if(degrau_atual == 0){
         return Count;
     }
-    Count++;
     return MEMO[degrau_atual][degraus_restantes];
 }
 
@@ -36,9 +43,17 @@ int degraus(int degrau_atual, int degraus_restantes){
 int main(int argc, char *argv[]){
     printf("Digite o numero de degraus: ");
     scanf("%d",&N);
+    printf("Digite o numero de restricoes: ");
+    scanf("%d",&NR);
+    VARG = (int*)malloc(NR*sizeof(int));
+    printf("Digite a lista de restricoes, seperando itens por espaços\n");
+    for(int i=0; i<NR; i++){
+        scanf("%d",&VARG[i]);
+    }
     MEMO = aloca_m_quad(N);
-    printf("Numero de possibilidades: %d\n",degraus(0,N-1));
+    printf("Numero de possibilidades: %d\n",degraus(0,N-1)-1);
     libera_m_quad(MEMO, N);
+    free(VARG);
     return 0;
 }
 
